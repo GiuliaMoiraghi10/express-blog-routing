24da42d440d67b26f1898abe701a5eff3b888b21
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-19 17:02:09 +0100

    Collego routers a app.js

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const port = 3000
 // Importo file posts
 const posts = require('./posts.js')
 
+// Importo file router
+const postsRouter = require('./routers/posts.js')
+
 //Espongo cartella public
 app.use(express.static('public'))
+app.use(express.static('router'))
 
 // Registro prima porta
 app.get('/', (req, res) => {
@@ -17,7 +21,10 @@ app.get('/', (req, res) => {
 })
 
 // Creo CRUD (che poi sposterò in altro file dentro cartella routers)
+// Copiato e incollato CRUD in file posts.js dentro a cartella router
 
+// Registro rotte collegate a router
+app.use('/posts', postsRouter)
 
 // Ascolto porta
 app.listen(port, () => {
