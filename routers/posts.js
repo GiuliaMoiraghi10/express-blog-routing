860091cd0f1f148df676397dac02bb2a85593600
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-19 16:27:06 +0100

    Modifico app in routers e tolgo istanze

--- routers/posts.js
+++ routers/posts.js
@@ -5,39 +5,39 @@ const express = require('express')
 const router = express.Router
 
 // INDEX
-app.get('/posts', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Lista dei posts AAAAAA')
 })
 
-app.get('/prova', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Lista dei posts prova')
 })
 
 // SHOW
-app.get('/posts/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id)
     res.send(`Dettagli del post con id ${id}`)
 })
 
 // STORE
-app.post('/posts', (req, res) => {
+router.post('/', (req, res) => {
     res.send('Creazione nuovo post')
 })
 
 // UPDATE
-app.put('/posts/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id)
     res.send(`Modifico il post con id ${id}`)
 })
 
 // MODIFY
-app.patch('/posts/:id', (req, res) => {
+router.patch('/:id', (req, res) => {
     const id = parseInt(req.params.id)
     res.send(`Modifico una parte del post con id ${id}`)
 })
 
 // DESTROY
-app.delete('/posts/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id)
     res.send(`Cancello il post con id ${id}`)
 })
